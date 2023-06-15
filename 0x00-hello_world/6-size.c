e964a99754d72b2b163e60c6feb6edde15203241
JosephAdegoke <adegokejoseph@15gmail.com>
2023-06-15 12:30:14 -0700

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("size of char: %dbyte(s)\n", sizeof(char));
+	printf("size of int: %dbyte(s)\n", sizeof(int));
+	printf("size of long int: $dbyte(s)\n", sizeof(long int));
+	printf("size of long long int: %dbyte(s)\n", sizeof(long long int));
+	printf("size of a float: %dbyte(s)\n", sizeof(float));
+	return (0);
+}
