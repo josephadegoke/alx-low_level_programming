fdd7368e27414e9983ccd526b392c74a81e86a20
JosephAdegoke <adegokejoseph@15gmail.com>
2023-06-15 14:09:49 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *
  *  Return 0 (success
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
+
 		return (0);
 }
