915131374c0a58282d9a3c88f26afc979a78cc41
JosephAdegoke <adegokejoseph@15gmail.com>
2023-06-17 10:57:18 -0700

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+* main - Prints numbers of base 16
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
