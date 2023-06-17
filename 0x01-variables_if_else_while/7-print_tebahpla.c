9021d01e1c9dd9926742002504047bb9a28e90e0
JosephAdegoke <adegokejoseph@15gmail.com>
2023-06-17 10:39:58 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * main - Prints lowercase letters in reverse
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char myLetters = "abcdefghijklmnopqrstuvwxyz";
+
+	for (myLetters <= 'z'; myLetters--)
+	{
+		putchar(myLetters);
+	}
+	putchar('\n');
+	return (0);
+}
