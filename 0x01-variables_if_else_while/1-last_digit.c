e381b0dc2cc419cb6215c4de028a7f3f5379c068
JosephAdegoke <adegokejoseph@15gmail.com>
2023-06-19 01:44:01 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number and whether it is
- * generated than 5, less than 6, or 0
+ * main - Prints the last digit of a randomly generated number
  *
  * Return: Always 0
  */
